b9a283d2eef00204deed46f822a6a26459452e3f
diweihua <weihua@teambition.com>
2017-05-15 16:30:58 +0800

    add Config::new and error deal

--- greprs/src/main.rs
+++ greprs/src/main.rs
@@ -1,17 +1,43 @@
 use std::env;
 use std::fs::File;
 use std::io::prelude::*;
+use std::process;
+
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        Ok(Config {
+            query: query,
+            filename: filename,
+        })
+    }
+}
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
 
-    println!("Searching for {}", query);
-    println!("In file {}\n", filename);
 
-    let mut f = File::open(filename).expect("file not found");
+    println!("Searching for {}", config.query);
+    println!("In file {}\n", config.filename);
+
+    let mut f = File::open(config.filename).expect("file not found");
 
     let mut contents = String::new();
     f.read_to_string(&mut contents).expect("something went wrong reading the file");
